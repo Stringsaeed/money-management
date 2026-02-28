a242e0465629d9579a22daf10a818d1d986355ef
Muhammed Ibrahim <stringsaeed@gmail.com>
2026-02-28 20:42:29 +0400

    chore: upgrade react-native-css to stable v3, move tailwind to devDeps, simplify metro config

--- metro.config.js
+++ metro.config.js
@@ -4,9 +4,4 @@ const { withNativewind } = require("nativewind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativewind(config, {
-  // inline variables break PlatformColor in CSS variables
-  inlineVariables: false,
-  // We add className support manually
-  globalClassNamePolyfill: false,
-});
+module.exports = withNativewind(config);
