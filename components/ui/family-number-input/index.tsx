f44a86d9872bcec4f7c79e6e5d27376a5d16ab23
Muhammed Ibrahim <stringsaeed@gmail.com>
2026-03-16 20:17:35 +0400

    chore: remove unused family-number-input component and add reanimated metro config

--- components/ui/family-number-input/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NumberInput } from "./number-input";
-
-const FamilyNumberInput = () => {
-  // @@TODO: rewrite this animation with Layout Animations
-  // The solution should be way easier than the current one
-  return <NumberInput />;
-};
-
-export { FamilyNumberInput };
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withUniwindConfig } = require("uniwind/metro");
+const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -7,7 +8,7 @@ const config = getDefaultConfig(__dirname);
 // Allow Metro to resolve .sql files for drizzle migrations
 config.resolver.sourceExts.push("sql");
 
-module.exports = withUniwindConfig(config, {
+module.exports = withUniwindConfig(wrapWithReanimatedMetroConfig(config), {
   cssEntryFile: "./global.css",
   dtsFile: "./uniwind-types.d.ts",
 });
