2486694d0424476323938f4d4105d1a87c2d1773
Muhammed Ibrahim <stringsaeed@gmail.com>
2026-05-30 02:53:10 +0400

    chore(expo): drop splash config override

--- app.config.ts
+++ app.config.ts
@@ -97,11 +97,6 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         projectId: "a33b24c0-b380-4d0a-8ce6-b2f4b61da346",
       },
     },
-    splash: {
-      image: "./assets/images/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#f9f8f6",
-    },
     updates: {
       url: "https://u.expo.dev/a33b24c0-b380-4d0a-8ce6-b2f4b61da346",
     },
